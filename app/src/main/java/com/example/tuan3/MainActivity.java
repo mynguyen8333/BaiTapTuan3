126035959631ccd4e4b1d8e767c6552269930a42
Admin <mynguyen8333@gmail.com>
2021-04-04 15:35:59 +0700

    lan3

--- app/src/main/java/com/example/tuan3/MainActivity.java
+++ app/src/main/java/com/example/tuan3/MainActivity.java
@@ -2,18 +2,63 @@ package com.example.tuan3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    Button btnChonMau;
+    private String text = null;
+    TextView txtTien;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView img = (ImageView) findViewById(R.id.imgAnh);
+        txtTien = findViewById(R.id.txtTien);
+        text = getIntent().getExtras().getString("Value");
+//        System.out.println(text);
+        if(text.equalsIgnoreCase(null)){
+            img.setImageResource(R.drawable.vsmart_xanh);
+            txtTien.setText("1.800.000đ");
+        }
+        else if(text.equalsIgnoreCase("Màu: xanh"))
+        {
+            img.setImageResource(R.drawable.vsmart_xanh);
+            txtTien.setText("1.800.000đ");
+        }else if(text.equalsIgnoreCase("Màu: đỏ"))
+        {
+            img.setImageResource(R.drawable.vs_red_a);
+            txtTien.setText("1.790.000đ");
+        }
+        else if(text.equalsIgnoreCase("Màu: bạc"))
+        {
+            img.setImageResource(R.drawable.vs_bac);
+            txtTien.setText("1.780.000đ");
+        }
+        else if(text.equalsIgnoreCase("Màu: đen"))
+        {
+            img.setImageResource(R.drawable.vsmart_black);
+            txtTien.setText("1.760.000đ");
+        }
 
+        btnChonMau = findViewById(R.id.btnChonMau);
+        btnChonMau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               ChonMau();
+            }
+        });
+
+    }
+
+    public void ChonMau(){
+        Intent intent = new Intent(this,Screen_2.class);
+        startActivity(intent);
     }
 
 
--- app/src/main/java/com/example/tuan3/Screen_2.java
+++ app/src/main/java/com/example/tuan3/Screen_2.java
@@ -2,28 +2,82 @@ package com.example.tuan3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class Screen_2 extends AppCompatActivity {
     Button btn_xanh,btn_do,btn_den,btn_xanhduong,btn_xong;
+    TextView txtMau,txtGia;
+    String text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_2);
-        btn_xanh = findViewById(R.id.button3);
-        btn_do = findViewById(R.id.button7);
-        btn_den = findViewById(R.id.button8);
-        btn_xanhduong = findViewById(R.id.button9);
-        btn_xong = findViewById(R.id.button10);
-
+        btn_xanh = findViewById(R.id.btnXanhDuong);
+        btn_do = findViewById(R.id.btnDo);
+        btn_den = findViewById(R.id.btnDen);
+        btn_xanhduong = findViewById(R.id.btnXanhDam);
+        btn_xong = findViewById(R.id.btnXong);
+        txtGia = findViewById(R.id.txtGia);
+        txtMau = findViewById(R.id.txtMau);
+        ImageView img = (ImageView) findViewById(R.id.imageDT);
         btn_xanh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                img.setImageResource(R.drawable.vs_bac);
+                txtMau.setText("Màu: bạc");
+                txtGia.setText("1.780.000đ");
             }
         });
 
+        btn_do.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setImageResource(R.drawable.vs_red_a);
+                txtMau.setText("Màu: đỏ");
+                txtGia.setText("1.790.000đ");
+            }
+        });
+
+        btn_den.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setImageResource(R.drawable.vsmart_black);
+                txtMau.setText("Màu: đen");
+                txtGia.setText("1.760.000đ");
+            }
+        });
+
+        btn_xanhduong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img.setImageResource(R.drawable.vsmart_xanh);
+                txtMau.setText("Màu: xanh");
+                txtGia.setText("1.800.000đ");
+            }
+        });
+
+        btn_xong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatHang();
+            }
+        });
     }
+
+
+    public void openDatHang(){
+        txtMau = findViewById(R.id.txtMau);
+        text = txtMau.getText().toString();
+//        System.out.println(text);
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.putExtra("Value",text);
+        startActivity(intent);
+    }
+
 }
